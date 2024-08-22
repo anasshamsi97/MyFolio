ea9421f55e3d4cf98ae5eff4693312e6321ed73d
Muhammad Anas <anasshamsi97@gmail.com>
2024-08-22 18:01:52 +0500

    Initial commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,5 @@
+import React, {useContext} from "react";
+import "./Footer.scss";
+import {Fade} from "react-reveal";
+import emoji from "react-easy-emoji";
+import StyleContext from "../../contexts/StyleContext";
